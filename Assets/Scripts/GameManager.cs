fd4effa453986613104e13df7fad9d5c401d51b4
F8 <thariqmirza94@gmail.com>
2025-10-01 17:27:59 -0700

    Spawner logic and base scriptable objects implemented Added all five enemy ScriptableObjects with correct stats and created matching cube prefabs with colored materials. Expanded the Spawner to filter by class or type, pick enemies using spawn rates, and instantiate the right prefab. Wired the scene with four spawn points, each using the correct filter and prefab list.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // List of both the enemy types and the spawners, so everything is in one place and organixzed
+    // List of both the enemy types and the spawners, so everything is in one place and organized
     public List<SO_EnemyType> enemyTypes;
     public List<Spawner> spawnPoints;
-    private SO_EnemyType tempType; // Temporary holder
 
     void Start()
     {
-        if (enemyTypes.Count == 0) // simple check to make sure there is something assigned
-            return;
-        tempType = enemyTypes[0];
-        for (int i = 0; i < spawnPoints.Count; i++) // Check to see if the spawner is working, will be modified later
-            spawnPoints[i].Spawn(tempType);
+        for (int i = 0; i < spawnPoints.Count; i++) // loops to find every enemy type
+        {
+            spawnPoints[i].Spawn(enemyTypes);
+        }
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,13 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal.VersionControl;
 using UnityEngine;
+
+public enum SpawnFilter// Restrictions for each spawner
+{
+    Any, ArcherOnly, GruntOnly, RedOnly
+}
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject enemyPrefab;
-    
-    public void Spawn(SO_EnemyType type) // Method that can be called in order to execute a spawn
+    public List<GameObject> enemyPrefabs;
+    public SpawnFilter filter;
+    private SO_EnemyType choice;
+
+    public void Spawn(List<SO_EnemyType> allTypes) // Method to check which spawner it is, and spawn the right units
     {
-        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        List<SO_EnemyType> options = new List<SO_EnemyType>();
+        for (int i = 0; i < allTypes.Count; i++) // For loop to assign the correct types based on the filter selected in inspector
+        {
+            SO_EnemyType temp = allTypes[i]; // temporary variable to loop through the types
+            if (filter == SpawnFilter.Any)
+                options.Add(temp);
+            else if (filter == SpawnFilter.ArcherOnly && temp.enemyClass == EnemyClass.Archer) 
+                options.Add(temp);
+            else if (filter == SpawnFilter.GruntOnly && temp.enemyClass == EnemyClass.Grunt) 
+                options.Add(temp);
+            else if (filter == SpawnFilter.RedOnly && temp.name == "RedType") 
+                options.Add(temp);
+        }
+        if (options.Count == 0) return; // Check to make sure for loop worked properly
+
+        choice = PickBySpawnRate(options);
+
+        GameObject prefabToSpawn = GetPrefab(choice);
+        if (prefabToSpawn == null) return; // Check to see if prefabs are assigned properly
+        
+        Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+    }
+    
+    /*
+     * Spawn Rate tells the game how likely each enemy is to appear
+     * Here, all the spawn rates are added up, and then a random number is picked.
+     * Whichever enemy's range it falls into is the one that spawns, that way the enemy with higher spawn rates get picked more often to spawn
+     */
+    SO_EnemyType PickBySpawnRate(List<SO_EnemyType> options) 
+    {
+        float totalRate = 0f;
+        for (int i = 0; i < options.Count; i++)
+        {
+            totalRate += options[i].spawnRate; // Total spawn rate within which we generate a random number
+        }
+
+        float randomNumber = Random.Range(0f, totalRate);
+
+        float currentRange = 0f;
+        for (int i = 0; i < options.Count; i++) // check which types range does the random number fall under, and return it
+        {
+            currentRange += options[i].spawnRate;
+            if (randomNumber <= currentRange)
+            {
+                return options[i];
+            }
+        }
+        
+        return options[0];
+    }
+    
+    GameObject GetPrefab(SO_EnemyType enemy) // Get the correct prefab based on the enemy, in case they have different models
+    {
+        GameObject prefab = null;
+
+        if (enemy.name == "RedType")
+        {
+            prefab = enemyPrefabs[0];
+        }
+        else if (enemy.name == "BrownType")
+        {
+            prefab = enemyPrefabs[1];
+        }
+        else if (enemy.name == "BlueType")
+        {
+            prefab = enemyPrefabs[2];
+        }
+        else if (enemy.name == "GreenType")
+        {
+            prefab = enemyPrefabs[3];
+        }
+        else if (enemy.name == "YellowType")
+        {
+            prefab = enemyPrefabs[4];
+        }
+
+        return prefab;
     }
 }
